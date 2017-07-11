13e288c586272f4df6328e82e5646f1f00fac7b5
CrazyHuiLiang <CrazyHuiLiang@163.com>
2017-07-11 09:32:44 +0800

    首页

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 /**
  * Created by ranwei on 2017/7/10.
  */
-
-
 import Vue from 'vue';
-import test from './components/homeList.vue';
-
+import app from './index.vue';
 new Vue({
-    el:'nav',
-    components:{test}
-
+    el:'[app]',
+    components:{
+       app
+    }
 });
 
---
+++ src/js/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <header>
+            <div class="header-add">+</div>
+            <div class="header-file">文件</div>
+            <div class="header-play">正在播放</div>
+        </header>
+        <nav>
+            <homeList></homeList>
+        </nav>
+        <footer>
+            <div class="footer-set">设置</div>
+            <div class="footer-compile">编辑</div>
+        </footer>
+    </div>
+</template>
+<script>
+import homeList from './components/homeList.vue';
+export default {
+    components: {
+       homeList 
+    }
+}
+</script>
+<style lang="scss" scoped>
+    header{
+        background: red;
+        padding: 5px;
+
+        div{
+            background: grey;
+        }
+    }
+    nav{
+        background: orange;
+    }
+    footer{
+        background: yellow;
+    }
+</style>
